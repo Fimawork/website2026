3a61f133ba072abdbe3da7da2dcf78e694408a0f
fima <fima.chou@gmail.com>
2025-08-03 12:23:30 +0800

    feat:新增threejs_tools工具。

--- main.js
+++ main.js
@@ -3,28 +3,46 @@ import * as THREE from 'three';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { Sky } from 'three/addons/objects/Sky.js';
+import {CameraManager,targetPosition,CameraDefaultPos, ControlsTargetDefaultPos} from './threejs_tools/fx_functions.js'
+
+let scene, camera, renderer, stats, mixer, clock;
+let cube;
 
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+init();
+animate();
+function init()
+{
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
 
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+  scene.background= new THREE.Color( 0xffffff );
 
-scene.background= new THREE.Color( 0xffffff );
+  camera.position.z = 5;
 
-camera.position.z = 5;
+  window.addEventListener( 'resize', onWindowResize );
+}
 
-function animate() {
+function onWindowResize() 
+{
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
 
+}
+
+function animate() 
+{
+  requestAnimationFrame( animate );
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
