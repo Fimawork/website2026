86dcb0d4db1d05e4984844440876aa07c7afe5db
fima <fima.chou@gmail.com>
2025-10-08 08:31:02 +0800

    feat:頁面調整(壓縮影片大小<100mb)。

---
+++ wordings.js
@@ -0,0 +1,47 @@
+
+export const Wording_P01 = 
+{
+    texture_img:'./textures/yan-ots-UuBR5kbvt4Y-unsplash.jpg',
+
+    opacity:1
+};
+
+export const Wording_P02 = 
+{
+    texture_img:'./textures/yan-ots-UuBR5kbvt4Y-unsplash.jpg',
+
+    opacity:1
+};
+
+export const Wording_P03 = 
+{
+    texture_img:'./textures/yan-ots-UuBR5kbvt4Y-unsplash.jpg',
+
+    opacity:1
+};
+
+export const Wording_P04 = 
+{
+    title_01:"互動性",
+    paragraph_01:"不只是瀏覽平面資訊，使用者可透過模型的旋轉、縮放等操作主動探索內容。",
+
+    title_02:"創新性",
+    paragraph_02:"透過場景化與動畫設計，以視覺敘事方式呈現品牌或產品，3D體驗更容易給使用者留下深刻印象。",
+
+    title_03:"延伸性",
+    paragraph_03:"產品設計、建築設計、訓練教學等，都能透過 3D 網頁呈現細節與體驗。"
+};
+
+export const Wording_P05 = 
+{
+    texture_img:'./textures/yan-ots-UuBR5kbvt4Y-unsplash.jpg',
+
+    opacity:1
+};
+
+export const Wording_P06 = 
+{
+    texture_img:'./textures/yan-ots-UuBR5kbvt4Y-unsplash.jpg',
+
+    opacity:1
+};
